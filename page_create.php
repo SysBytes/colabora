356a20e620754841e36dbc8785181718ab23b16b
Oscar Cánchica <oscar.canchica@grupoadmark.com>
2018-06-22 14:00:38 -0500

    Actualizacion Galeria de Imagenes

--- page_create.php
+++ page_create.php
@@ -15,14 +15,45 @@
 				<div class="content" id="galeria_fotos">
 					<h3 class="section-title">Fotos</h3>
 					<div id="g-fotos" style="display:none;">
-						<img alt="Image 1 Title" src="img/galeria/foto-1.jpg" data-image="img/galeria/foto-1.jpg" data-description="Image 1 Description">
-						<img alt="Image 2 Title" src="img/galeria/foto-2.jpg" data-image="img/galeria/foto-2.jpg" data-description="Image 2 Description">
-						<img alt="Image 3 Title" src="img/galeria/foto-3.jpg" data-image="img/galeria/foto-3.jpg" data-description="Image 3 Description">
-						<img alt="Image 4 Title" src="img/galeria/foto-4.jpg" data-image="img/galeria/foto-4.jpg" data-description="Image 4 Description">
-						<img alt="Image 5 Title" src="img/galeria/foto-5.jpg" data-image="img/galeria/foto-5.jpg" data-description="Image 5 Description">
-						<img alt="Image 6 Title" src="img/galeria/foto-6.jpg" data-image="img/galeria/foto-6.jpg" data-description="Image 6 Description">
-						<img alt="Image 1 Title" src="img/galeria/foto-1.jpg" data-image="img/galeria/foto-1.jpg" data-description="Image 1 Description">
-						<img alt="Image 2 Title" src="img/galeria/foto-2.jpg" data-image="img/galeria/foto-2.jpg" data-description="Image 2 Description">
+						<img alt="Colabora Space" src="img/galeria/1.jpg" data-image="img/galeria/1.jpg">
+						<img alt="Colabora Space" src="img/galeria/2.jpg" data-image="img/galeria/2.jpg">
+						<img alt="Colabora Space" src="img/galeria/3.jpg" data-image="img/galeria/3.jpg">
+						<img alt="Colabora Space" src="img/galeria/4.jpg" data-image="img/galeria/4.jpg">
+						<img alt="Colabora Space" src="img/galeria/5.jpg" data-image="img/galeria/5.jpg">
+						<img alt="Colabora Space" src="img/galeria/6.jpg" data-image="img/galeria/6.jpg">
+						<img alt="Colabora Space" src="img/galeria/7.jpg" data-image="img/galeria/7.jpg">
+						<img alt="Colabora Space" src="img/galeria/8.jpg" data-image="img/galeria/8.jpg">
+						<img alt="Colabora Space" src="img/galeria/9.jpg" data-image="img/galeria/9.jpg">
+						<img alt="Colabora Space" src="img/galeria/10.jpg" data-image="img/galeria/10.jpg">
+						<img alt="Colabora Space" src="img/galeria/11.jpg" data-image="img/galeria/11.jpg">
+						<img alt="Colabora Space" src="img/galeria/12.jpg" data-image="img/galeria/12.jpg">
+						<img alt="Colabora Space" src="img/galeria/13.jpg" data-image="img/galeria/13.jpg">
+						<img alt="Colabora Space" src="img/galeria/14.jpg" data-image="img/galeria/14.jpg">
+						<img alt="Colabora Space" src="img/galeria/15.jpg" data-image="img/galeria/15.jpg">
+						<img alt="Colabora Space" src="img/galeria/16.jpg" data-image="img/galeria/16.jpg">
+						<img alt="Colabora Space" src="img/galeria/17.jpg" data-image="img/galeria/17.jpg">
+						<img alt="Colabora Space" src="img/galeria/18.jpg" data-image="img/galeria/18.jpg">
+						<img alt="Colabora Space" src="img/galeria/19.jpg" data-image="img/galeria/19.jpg">
+						<img alt="Colabora Space" src="img/galeria/20.jpg" data-image="img/galeria/20.jpg">
+						<img alt="Colabora Space" src="img/galeria/21.jpg" data-image="img/galeria/21.jpg">
+						<img alt="Colabora Space" src="img/galeria/22.jpg" data-image="img/galeria/22.jpg">
+						<img alt="Colabora Space" src="img/galeria/23.jpg" data-image="img/galeria/23.jpg">
+						<img alt="Colabora Space" src="img/galeria/24.jpg" data-image="img/galeria/24.jpg">
+						<img alt="Colabora Space" src="img/galeria/25.jpg" data-image="img/galeria/25.jpg">
+						<img alt="Colabora Space" src="img/galeria/26.jpg" data-image="img/galeria/26.jpg">
+						<img alt="Colabora Space" src="img/galeria/27.jpg" data-image="img/galeria/27.jpg">
+						<img alt="Colabora Space" src="img/galeria/28.jpg" data-image="img/galeria/28.jpg">
+						<img alt="Colabora Space" src="img/galeria/29.jpg" data-image="img/galeria/29.jpg">
+						<img alt="Colabora Space" src="img/galeria/30.jpg" data-image="img/galeria/30.jpg">
+						<img alt="Colabora Space" src="img/galeria/31.jpg" data-image="img/galeria/31.jpg">
+						<img alt="Colabora Space" src="img/galeria/32.jpg" data-image="img/galeria/32.jpg">
+						<img alt="Colabora Space" src="img/galeria/33.jpg" data-image="img/galeria/33.jpg">
+						<img alt="Colabora Space" src="img/galeria/34.jpg" data-image="img/galeria/34.jpg">
+						<img alt="Colabora Space" src="img/galeria/35.jpg" data-image="img/galeria/35.jpg">
+						<img alt="Colabora Space" src="img/galeria/36.jpg" data-image="img/galeria/36.jpg">
+						<img alt="Colabora Space" src="img/galeria/37.jpg" data-image="img/galeria/37.jpg">
+						<img alt="Colabora Space" src="img/galeria/38.jpg" data-image="img/galeria/38.jpg">
+						<img alt="Colabora Space" src="img/galeria/39.jpg" data-image="img/galeria/39.jpg">
 					</div>
 				</div>
 
